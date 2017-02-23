ae0fc88fa77cea80a4e62a7d7c9f1edcb4fa8bec
ArialBlack <andreyturik@gmail.com>
2017-02-23 16:31:13 +0200

    save

--- build-front/gulpfile.js
+++ build-front/gulpfile.js
@@ -7,8 +7,14 @@ var copy = require('gulp-copy');
 gulp.task('copy', function() {
     gulp.src(['source/node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js'])
         .pipe(gulp.dest('../themes/bootstrap/js/'))
+    gulp.src(['source/node_modules/instafeed.js/instafeed.min.js'])
+        .pipe(gulp.dest('../themes/bootstrap/gg_project/js/'))
     gulp.src(['source/node_modules/bootstrap-sass/assets/fonts/**/*'])
         .pipe(gulp.dest('../themes/bootstrap/fonts/'))
+    gulp.src(['source/node_modules/font-awesome/fonts/**/*'])
+        .pipe(gulp.dest('../themes/bootstrap/gg_project/fonts/'))
+    gulp.src(['source/node_modules/font-awesome/css/font-awesome.min.css'])
+        .pipe(gulp.dest('../themes/bootstrap/gg_project/css/'))
 });
 
 gulp.task('sass', function () {
