6e736369417217c8202934558a4435f87a5f6800
ArialBlack <andreyturik@gmail.com>
2017-02-23 17:59:25 +0200

    add grunt

---
+++ themes/ggproject_ui/js/main.js
@@ -0,0 +1,9 @@
+(function($){
+    $(function() {
+
+        $( document ).ready(function() {
+console.log('---------------run!');
+        });
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
