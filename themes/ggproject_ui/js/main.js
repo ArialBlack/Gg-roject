6f67604661f32ecc680fae425820c7ea31e4cac4
ArialBlack <andreyturik@gmail.com>
2017-02-28 21:08:16 +0200

    fixes

--- themes/ggproject_ui/js/main.js
+++ themes/ggproject_ui/js/main.js
@@ -9,18 +9,7 @@
             });
         }
 
-
-        $(document).ready(function() {
-            console.log('---------------run!');
-            $('.carousel').carousel();
-
-            var userFeed = new Instafeed({
-                get: 'user',
-                userId: '1c6f2094d1f344d28b3f38cc5a9bcaf9',
-                accessToken: '1365268559.4eb71ba.e07e6cd904b54f0994e586d32fa8365b'
-            });
-            userFeed.run();
-            // insta-widget-script
+        function buildInstagram() {
             var token = '1365268559.4eb71ba.e07e6cd904b54f0994e586d32fa8365b', // learn how to obtain it below
                 userid = 1365268559, // User ID - get it in source HTML of your Instagram profile or look at the next example :)
                 num_photos = 16; // how much photos do you want to get
@@ -35,7 +24,7 @@
                         $('#instafeed').append('<li><img src="' + data.data[x].images.low_resolution.url + '"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
                         // data.data[x].images.thumbnail.url - URL of image 150х150
                         // data.data[x].images.standard_resolution.url - URL of image 612х612
-                        // data.data[x].link - Instagram post URL 
+                        // data.data[x].link - Instagram post URL
                     }
                     $('#instafeed').addClass('clearfix');
                 },
@@ -43,10 +32,11 @@
                     console.log(data); // send the error notifications to console
                 }
             });
+        }
 
-            //end
-
-
+        $(document).ready(function() {
+            $('.carousel').carousel();
+            buildInstagram();
         });
 
         $(window).load(function() {
